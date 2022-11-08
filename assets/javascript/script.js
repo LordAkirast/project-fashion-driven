601480978cf7ad2b88dde0889ef88593bdffee38
LordAkirast <darkwarish@gmail.com>
2022-11-08 19:46:47 -0300

    Selection logic implemented | Last Orders started

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,4 +1,7 @@
 
+
+document.getElementById('btn_confirm').onclick = null;
+
 let tshirt = 0;
 let camiseta = 0;
 let mangalonga = 0;
@@ -12,16 +15,96 @@ function choose1(selected) {
     tshirt = 1;
     camiseta = 0;
     mangalonga = 0;
+    confirmButton();
     } else if (document.getElementById('camiseta').classList.contains('confirm')) {
         camiseta = 1;
         tshirt = 0;
         mangalonga = 0;
+        confirmButton();
     } else if (document.getElementById('mangalonga').classList.contains('confirm')) {
         mangalonga = 1;
         tshirt = 0;
         camiseta = 0
+        confirmButton();
     }
     console.log(tshirt,camiseta,mangalonga)
 
 } 
+
+
+let golav = 0;
+let golaredonda = 0;
+let golapolo = 0;
+function choose2(selected) {
+    document.getElementById('golav').classList.remove('confirm')
+    document.getElementById('golaredonda').classList.remove('confirm')
+    document.getElementById('golapolo').classList.remove('confirm')
+    selected.classList.toggle('confirm')
+
+    if (document.getElementById('golav').classList.contains('confirm')) {
+        golav = 1;
+        golaredonda = 0;
+        golapolo = 0;
+        confirmButton();
+    } else if (document.getElementById('golaredonda').classList.contains('confirm')) {
+        golav = 0;
+        golaredonda = 1;
+        golapolo = 0;
+        confirmButton();
+    } else if (document.getElementById('golapolo').classList.contains('confirm')) {
+        golav = 0;
+        golaredonda = 0;
+        golapolo = 1;
+        confirmButton();
+    }
+    console.log(golav,golaredonda,golapolo)
+
+} 
+
+
+
+let seda = 0;
+let algodao = 0;
+let poliester = 0;
+function choose3(selected) {
+    document.getElementById('seda').classList.remove('confirm')
+    document.getElementById('algodao').classList.remove('confirm')
+    document.getElementById('poliester').classList.remove('confirm')
+    selected.classList.toggle('confirm')
+
+    if (document.getElementById('seda').classList.contains('confirm')) {
+        seda = 1;
+        algodao = 0;
+        poliester = 0;
+        confirmButton();
+    } else if (document.getElementById('algodao').classList.contains('confirm')) {
+        seda = 0;
+        algodao = 1;
+        poliester = 0;
+        confirmButton();
+    } else if (document.getElementById('poliester').classList.contains('confirm')) {
+        seda = 0;
+        algodao = 0;
+        poliester = 1;
+        confirmButton();
+    }
+    console.log(seda,algodao,poliester)
+
+} 
+
+function confirmButton() {
+    if(tshirt + camiseta + mangalonga + golav + golaredonda + golapolo + seda + algodao + poliester === 3) {
+        document.getElementById('btn_confirm').classList.add('button_confirm');
+        document.getElementById('btn_confirm').onclick = proceed;
+
+    }
+}
+
+
+function proceed() {
+    element = document.querySelector('.last_orders')
+    element.innerHTML += '<div class="order"> <img src="./assets/images/Blusa1.png" alt="Blusa1"><p>Criador: Agata</p></div>'
+}
+
+///o que fazer? replicar isso para os outros.
 
